3d63ec783be45b510eec20f3d68f547dc2b54d21
DnNBoss <std.dnnboss@gmail.com>
2026-02-15 14:45:22 +0300

    Initial Commit

---
+++ Streakly/StreaklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreaklyApp.swift
+//  Streakly
+//
+//  Created by Дмитрий Козлов on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct StreaklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
