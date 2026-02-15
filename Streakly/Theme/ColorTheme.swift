7e879b77eeb9f470f06b9066e6d3f5aa97309050
DnNBoss <std.dnnboss@gmail.com>
2026-02-15 20:32:09 +0300

    Add custom colors extension

---
+++ Streakly/Theme/ColorTheme.swift
@@ -0,0 +1,22 @@
+//
+//  ColorTheme.swift
+//  Streakly
+//
+//  Created by Дмитрий Козлов on 15.02.26.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var lightWhite: Color {
+        Color(red: 241 / 255, green: 240 / 255, blue: 247 / 255)
+    }
+    
+    static var lightDark: Color {
+        Color(red: 27 / 255, green: 31 / 255, blue: 42 / 255)
+    }
+    
+    static var lightGreen: Color {
+        Color(red: 211 / 255, green: 255 / 255, blue: 88 / 255)
+    }
+}
