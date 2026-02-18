d5f173b8dba4c9542705720bc544a0a392256841
DnNBoss <std.dnnboss@gmail.com>
2026-02-18 22:19:17 +0300

    Add week strip views

---
+++ Streakly/View/WeekStripView.swift
@@ -0,0 +1,37 @@
+//
+//  WeekStripView.swift
+//  Streakly
+//
+//  Created by Дмитрий Козлов on 18.02.26.
+//
+
+import SwiftUI
+
+struct WeekStripView: View {
+    @State private var selectedDate: Date = .now
+    
+    private var weekDates: [Date] {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: .now)
+        
+        return (-3...3).compactMap {
+            calendar.date(byAdding: .day, value: $0, to: today)
+        }
+    }
+    
+    var body: some View {
+        HStack {
+            ForEach(weekDates, id: \.self) { date in
+                DayCellView(date: date,
+                            isSelected: Calendar.current.isDate(date, inSameDayAs: selectedDate))
+                .onTapGesture {
+                    selectedDate = date
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    WeekStripView()
+}
